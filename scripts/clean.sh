a34c36e4970776c5dc574f6e99332ae4bb1210b0
DavideScassola <davide.scassola1@gmail.com>
2026-02-07 16:03:15 +0100

    struture presentation

---
+++ scripts/clean.sh
@@ -0,0 +1,1 @@
+find . -type f \( -name "*.aux" -o -name "*.log" -o -name "*.out" -o -name "*.toc" -o -name "*.lof" -o -name "*.lot" -o -name "*.fls" -o -name "*.fdb_latexmk" -o -name "*.synctex.gz" -o -name "*.bbl" -o -name "*.blg" -o -name "*.bcf" -o -name "*.run.xml" -o -name "*.nav" -o -name "*.snm" -o -name "*.vrb" -o -name "*.fot" -o -name "*.fmt" \) -delete
